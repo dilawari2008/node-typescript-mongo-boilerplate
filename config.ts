6fc71fb5519fbc9a6edf20c429a9981b222d3f73
vagish@fibr.ai <vagish@fibr.ai>
2025-04-20 20:21:37 +0530

    Revert "Tests"
    This reverts commit f4e1494fff84a53cee5a86f1e1d46b487406eb68.

--- config.ts
+++ config.ts
@@ -11,9 +11,6 @@ const Config = {
   },
   jwtSecret: process.env.JWT_SECRET || '',
   environment: process.env.ENV || Environments.development,
-  ordersFile: process.env.ORDERS_FILE || '../../files/orders.json',
-  orderbookFile: process.env.ORDERBOOK_FILE || '../../files/orderbook.json',
-  tradesFile: process.env.TRADES_FILE || '../../files/trades.json',
 };
 
 export default Config
